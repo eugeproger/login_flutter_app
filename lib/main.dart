5d381b64c5ed7d2b7b6568d2639a478d8a38b4ba
Euge <euge.itech@gmail.com>
2023-09-25 18:20:40 +0200

    create Flutter Native Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -23,23 +23,8 @@ class AppHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text(".appable/"), leading: const Icon(Icons.ondemand_video)),
-      floatingActionButton: FloatingActionButton(onPressed: () {  }, child: const Icon(Icons.add_shopping_cart_outlined),),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: ListView(
-          children: [
-             Text("Heading", style: Theme.of(context).textTheme.headline2,),
-             Text("Sub-heading", style: Theme.of(context).textTheme.subtitle2,),
-             Text("Paragraph", style: Theme.of(context).textTheme.bodyText1),
-            ElevatedButton(onPressed: () {}, child: const Text("Elevated Button"),),
-            OutlinedButton(onPressed: () {}, child: const Text("Outlined Button"),),
-             const Padding(padding: EdgeInsets.all(20.0),
-                child: Image(image: AssetImage("assets/images/books.png")),
-             ),
-          ],
-        ),
-      ),
+      appBar: AppBar(title: const Text(".appable/")),
+      body: const Center(child: Text("Home Page")),
     );
   }
 }
